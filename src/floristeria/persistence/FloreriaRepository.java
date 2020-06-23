41d4ef7a160b04b089b99ad9d2dd8a610b2cb6cb
Ivan <ielizalde@gmail.com>
2020-06-23 11:55:38 +0200

    First commit Ivan

--- src/floristeria/persistence/FloreriaRepository.java
+++ src/floristeria/persistence/FloreriaRepository.java
@@ -1,4 +1,17 @@
 package floristeria.persistence;
 
+import floristeria.domain.Producte;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FloreriaRepository {
+
+    //lista de todos los productos
+    private static List<Producte> items = new ArrayList<>();
+
+    //Constructor de la lista
+    public FloreriaRepository() {
+
+    }
 }
