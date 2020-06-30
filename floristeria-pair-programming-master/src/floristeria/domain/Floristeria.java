51fa6c8a6eb35a0ff7e024d3bf9c5965e536e39e
Elena Piaggio <elenapiaggio@gmail.com>
2020-06-30 10:28:37 +0200

    UPDATE floristeria class

--- floristeria-pair-programming-master/src/floristeria/domain/Floristeria.java
+++ floristeria-pair-programming-master/src/floristeria/domain/Floristeria.java
@@ -1,4 +1,22 @@
 package floristeria.domain;
 
 public class Floristeria {
+    private String name;
+
+    public Floristeria(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Floristeria{" + "name='" + name + '\'' +  '}';
+    }
 }
